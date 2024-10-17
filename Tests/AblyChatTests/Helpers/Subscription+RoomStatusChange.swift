ably-labs/ably-chat-swift

commit 25e5052739212b9ce058a677d906d60ee56cf820

Start implementing room lifecycle spec
This is based on [1] at b4a495e. It’s generated some questions, which
I’ve asked on that PR.

Note that the spec has been through a few revisions since I started
implementing this; I’ve tried to keep everything in sync but some older
stuff might still be accidentally there.

I’ve implemented most of the specified behaviour for the ATTACH, DETACH,
and RELEASE operations. I have not yet implemented RETRY since it’s
quite different to those three and I wanted to get eyes on this first
chunk of work; have created #51 for implementing it.

Of the operations that I have implemented, I have not implemented the
following (this is accurately reflected by the @spec… tags in the
tests):

- Lifecycle behaviour that relates to another ongoing operation (created
  #52)

- Lifecycle behaviour relating to “transient disconnect timeouts”
  (created #48)

- Lifecycle behaviour that occurs “asynchronously” outside an operation
  (created #50)

- CHA-RL1g2, which refers to emitting “discontinuity events” which are a
  concept not yet implemented; this will come in #53.

The room lifecycle manager introduced by this commit is currently a
standalone object, which is not integrated with the rest of the SDK.
I’ve created #47 for doing this integration work.

The @spec… tags are based on the on the JS rules [2] @ 8c9ce8b, but I
camel-cased them and also decided that:

- if a test doesn't relate to a spec point, it doesn't need any markers

- there should be a way to know that a spec point is tested across
  multiple tests

- there should be a way of marking a spec point as implemented but not
  tested (so that can show up differently in reporting; important given
  that we’re also going to use this report as a to-do list of what still
  needs to be implemented)

Part of #28.

[1] https://github.com/ably/specification/pull/200
[2] https://github.com/ably/ably-js/blob/main/CONTRIBUTING.md#tests-alignment-with-the-ably-features-specification

Sources/AblyChat/RoomLifecycleManager.swift
@@ -1,0 +1,297 @@
+import Ably
+
+/// The interface that the lifecycle manager expects its contributing realtime channels to conform to.
+///
+/// We use this instead of the ``RealtimeChannel`` interface as its ``attach`` and ``detach`` methods are `async` instead of using callbacks. This makes it easier to write mocks for (since ``RealtimeChannel`` doesn’t express to the type system that the callbacks it receives need to be `Sendable`, it’s hard to, for example, create a mock that creates a `Task` and then calls the callback from inside this task).
+///
+/// We choose to also mark the channel’s mutable state as `async`. This is a way of highlighting at the call site of accessing this state that, since `ARTRealtimeChannel` mutates this state on a separate thread, it’s possible for this state to have changed since the last time you checked it, or since the last time you performed an operation that might have mutated it, or since the last time you recieved an event informing you that it changed. To be clear, marking these as `async` doesn’t _solve_ these issues; it just makes them a bit more visible. We’ll decide how to address them in https://github.com/ably-labs/ably-chat-swift/issues/49.
+internal protocol RoomLifecycleContributorChannel: Sendable {
+    func attach() async throws(ARTErrorInfo)
+    func detach() async throws(ARTErrorInfo)
+
+    var state: ARTRealtimeChannelState { get async }
+    var errorReason: ARTErrorInfo? { get async }
+}
+
+internal actor RoomLifecycleManager<Channel: RoomLifecycleContributorChannel> {
+    /// A realtime channel that contributes to the room lifecycle.
+    internal struct Contributor {
+        /// The room feature that this contributor corresponds to. Used only for choosing which error to throw when a contributor operation fails.
+        internal var feature: RoomFeature
+
+        internal var channel: Channel
+    }
+
+    internal private(set) var current: RoomLifecycle
+    internal private(set) var error: ARTErrorInfo?
+
+    private let logger: InternalLogger
+    private let clock: SimpleClock
+    private let contributors: [Contributor]
+
+    internal init(
+        contributors: [Contributor],
+        logger: InternalLogger,
+        clock: SimpleClock
+    ) {
+        self.init(
+            current: nil,
+            contributors: contributors,
+            logger: logger,
+            clock: clock
+        )
+    }
+
+    #if DEBUG
+        internal init(
+            testsOnly_current current: RoomLifecycle? = nil,
+            contributors: [Contributor],
+            logger: InternalLogger,
+            clock: SimpleClock
+        ) {
+            self.init(
+                current: current,
+                contributors: contributors,
+                logger: logger,
+                clock: clock
+            )
+        }
+    #endif
+
+    private init(
+        current: RoomLifecycle?,
+        contributors: [Contributor],
+        logger: InternalLogger,
+        clock: SimpleClock
+    ) {
+        self.current = current ?? .initialized
+        self.contributors = contributors
+        self.logger = logger
+        self.clock = clock
+    }
+
+    // TODO: clean up old subscriptions (https://github.com/ably-labs/ably-chat-swift/issues/36)
+    private var subscriptions: [Subscription<RoomStatusChange>] = []
+
+    internal func onChange(bufferingPolicy: BufferingPolicy) -> Subscription<RoomStatusChange> {
+        let subscription: Subscription<RoomStatusChange> = .init(bufferingPolicy: bufferingPolicy)
+        subscriptions.append(subscription)
+        return subscription
+    }
+
+    /// Updates ``current`` and ``error`` and emits a status change event.
+    private func changeStatus(to new: RoomLifecycle, error: ARTErrorInfo? = nil) {
+        logger.log(message: "Transitioning from \(current) to \(new), error \(String(describing: error))", level: .info)
+        let previous = current
+        current = new
+        self.error = error
+        let statusChange = RoomStatusChange(current: current, previous: previous, error: error)
+        emitStatusChange(statusChange)
+    }
+
+    private func emitStatusChange(_ change: RoomStatusChange) {
+        for subscription in subscriptions {
+            subscription.emit(change)
+        }
+    }
+
+    /// Implements CHA-RL1’s `ATTACH` operation.
+    internal func performAttachOperation() async throws {
+        switch current {
+        case .attached:
+            // CHA-RL1a
+            return
+        case .releasing:
+            // CHA-RL1b
+            throw ARTErrorInfo(chatError: .roomIsReleasing)
+        case .released:
+            // CHA-RL1c
+            throw ARTErrorInfo(chatError: .roomIsReleased)
+        case .initialized, .suspended, .attaching, .detached, .detaching, .failed:
+            break
+        }
+
+        // CHA-RL1e
+        changeStatus(to: .attaching)
+
+        // CHA-RL1f
+        for contributor in contributors {
+            do {
+                logger.log(message: "Attaching contributor \(contributor)", level: .info)
+                try await contributor.channel.attach()
+            } catch let contributorAttachError {
+                let contributorState = await contributor.channel.state
+                logger.log(message: "Failed to attach contributor \(contributor), which is now in state \(contributorState), error \(contributorAttachError)", level: .info)
+
+                switch contributorState {
+                case .suspended:
+                    // CHA-RL1h2
+                    let error = ARTErrorInfo(chatError: .attachmentFailed(feature: contributor.feature, underlyingError: contributorAttachError))
+                    changeStatus(to: .suspended, error: error)
+
+                    // CHA-RL1h3
+                    throw error
+                case .failed:
+                    // CHA-RL1h4
+                    let error = ARTErrorInfo(chatError: .attachmentFailed(feature: contributor.feature, underlyingError: contributorAttachError))
+                    changeStatus(to: .failed, error: error)
+
+                    // CHA-RL1h5
+                    // TODO: Implement the "asynchronously with respect to CHA-RL1h4" part of CHA-RL1h5 (https://github.com/ably-labs/ably-chat-swift/issues/50)
+                    await detachNonFailedContributors()
+
+                    throw error
+                default:
+                    // TODO: The spec assumes the channel will be in one of the above states, but working in a multi-threaded environment means it might not be (https://github.com/ably-labs/ably-chat-swift/issues/49)
+                    preconditionFailure("Attach failure left contributor in unexpected state \(contributorState)")
+                }
+            }
+        }
+
+        // CHA-RL1g1
+        changeStatus(to: .attached)
+    }
+
+    /// Implements CHA-RL1h5’s "detach all channels that are not in the FAILED state".
+    private func detachNonFailedContributors() async {
+        for contributor in contributors where await (contributor.channel.state) != .failed {
+            // CHA-RL1h6: Retry until detach succeeds
+            while true {
+                do {
+                    logger.log(message: "Detaching non-failed contributor \(contributor)", level: .info)
+                    try await contributor.channel.detach()
+                    break
+                } catch {
+                    logger.log(message: "Failed to detach non-failed contributor \(contributor), error \(error). Retrying.", level: .info)
+                    // Loop repeats
+                }
+            }
+        }
+    }
+
+    /// Implements CHA-RL2’s DETACH operation.
+    internal func performDetachOperation() async throws {
+        switch current {
+        case .detached:
+            // CHA-RL2a
+            return
+        case .releasing:
+            // CHA-RL2b
+            throw ARTErrorInfo(chatError: .roomIsReleasing)
+        case .released:
+            // CHA-RL2c
+            throw ARTErrorInfo(chatError: .roomIsReleased)
+        case .failed:
+            // CHA-RL2d
+            throw ARTErrorInfo(chatError: .roomInFailedState)
+        case .initialized, .suspended, .attaching, .attached, .detaching:
+            break
+        }
+
+        // CHA-RL2e
+        changeStatus(to: .detaching)
+
+        // CHA-RL2f
+        var firstDetachError: Error?
+        for contributor in contributors {
+            logger.log(message: "Detaching contributor \(contributor)", level: .info)
+            do {
+                try await contributor.channel.detach()
+            } catch {
+                let contributorState = await contributor.channel.state
+                logger.log(message: "Failed to detach contributor \(contributor), which is now in state \(contributorState), error \(error)", level: .info)
+
+                switch contributorState {
+                case .failed:
+                    // CHA-RL2h1
+                    guard let contributorError = await contributor.channel.errorReason else {
+                        // TODO: The spec assumes this will be populated, but working in a multi-threaded environment means it might not be (https://github.com/ably-labs/ably-chat-swift/issues/49)
+                        preconditionFailure("Contributor entered FAILED but its errorReason is not set")
+                    }
+
+                    let error = ARTErrorInfo(chatError: .detachmentFailed(feature: contributor.feature, underlyingError: contributorError))
+
+                    if firstDetachError == nil {
+                        // We’ll throw this after we’ve tried detaching all the channels
+                        firstDetachError = error
+                    }
+
+                    // This check is CHA-RL2h2
+                    if current != .failed {
+                        changeStatus(to: .failed, error: error)
+                    }
+                default:
+                    // CHA-RL2h3: Retry until detach succeeds, with a pause before each attempt
+                    while true {
+                        do {
+                            logger.log(message: "Will attempt to detach non-failed contributor \(contributor) in 1s.", level: .info)
+                            // TODO: what's the correct wait time? (https://github.com/ably/specification/pull/200#discussion_r1763799223)
+                            try await clock.sleep(timeInterval: 1)
+                            logger.log(message: "Detaching non-failed contributor \(contributor)", level: .info)
+                            try await contributor.channel.detach()
+                            break
+                        } catch {
+                            // Loop repeats
+                            logger.log(message: "Failed to detach non-failed contributor \(contributor), error \(error). Will retry.", level: .info)
+                        }
+                    }
+                }
+            }
+        }
+
+        if let firstDetachError {
+            // CHA-RL2f
+            throw firstDetachError
+        }
+
+        // CHA-RL2g
+        changeStatus(to: .detached)
+    }
+
+    /// Implementes CHA-RL3’s RELEASE operation.
+    internal func performReleaseOperation() async {
+        switch current {
+        case .released:
+            // CHA-RL3a
+            return
+        case .detached:
+            // CHA-RL3b
+            changeStatus(to: .released)
+            return
+        case .releasing, .initialized, .attached, .attaching, .detaching, .suspended, .failed:
+            break
+        }
+
+        changeStatus(to: .releasing)
+
+        // CHA-RL3d
+        for contributor in contributors {
+            while true {
+                let contributorState = await contributor.channel.state
+
+                // CHA-RL3e
+                guard contributorState != .failed else {
+                    logger.log(message: "Contributor \(contributor) is FAILED; skipping detach", level: .info)
+                    break
+                }
+
+                logger.log(message: "Detaching contributor \(contributor)", level: .info)
+                do {
+                    try await contributor.channel.detach()
+                    break
+                } catch {
+                    // CHA-RL3f: Retry until detach succeeds, with a pause before each attempt
+                    logger.log(message: "Failed to detach contributor \(contributor), error \(error). Will retry in 1s.", level: .info)
+                    // TODO: Make this not trap in the case where the Task is cancelled (as part of the broader https://github.com/ably-labs/ably-chat-swift/issues/29 for handling task cancellation)
+                    // TODO: what's the correct wait time? (https://github.com/ably/specification/pull/200#discussion_r1763822207)
+                    // swiftlint:disable:next force_try
+                    try! await clock.sleep(timeInterval: 1)
+                    // Loop repeats
+                }
+            }
+        }
+
+        // CHA-RL3g
+        changeStatus(to: .released)
+    }
+}

commit bc510bdc2b76011884c430ed7935d384fc32350c

Split room lifecycle “attaching” status in two
This will be used for implementing CHA-RL4b7 (the first such point in
the spec referenced in 25e5052, which accidentally contains two spec
points with this identifier), which allows a contributor state change to
put the room into an ATTACHING status outside of the context of a room
lifecycle operation.

In this situation, it may also wish to attach an error to the ATTACHED
room status, so update the public API to reflect this.

Example/AblyChatExample/ContentView.swift
@@ -172,7 +172,7 @@ struct ContentView: View {
     func showRoomStatus() async throws {
         for await status in try await room().status.onChange(bufferingPolicy: .unbounded) {
             withAnimation {
-                if status.current == .attaching {
+                if status.current.isAttaching {
                     statusInfo = "\(status.current)...".capitalized
                 } else {
                     statusInfo = "\(status.current)".capitalized

Example/AblyChatExample/Mocks/MockClients.swift
@@ -412,7 +412,7 @@ actor MockRoomStatus: RoomStatus {
 
     private func createSubscription() -> MockSubscription<RoomStatusChange> {
         let subscription = MockSubscription<RoomStatusChange>(randomElement: {
-            RoomStatusChange(current: [.attached, .attached, .attached, .attached, .attaching, .attaching, .suspended(error: .createUnknownError())].randomElement()!, previous: .attaching)
+            RoomStatusChange(current: [.attached, .attached, .attached, .attached, .attaching(error: nil), .attaching(error: nil), .suspended(error: .createUnknownError())].randomElement()!, previous: .attaching(error: nil))
         }, interval: 8)
         mockSubscriptions.append(subscription)
         return subscription

Sources/AblyChat/RoomLifecycleManager.swift
@@ -139,7 +139,8 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
 
     internal enum Status: Equatable {
         case initialized
-        case attaching(attachOperationID: UUID)
+        case attachingDueToAttachOperation(attachOperationID: UUID)
+        case attachingDueToContributorStateChange(error: ARTErrorInfo?)
         case attached
         case detaching(detachOperationID: UUID)
         case detached
@@ -152,8 +153,10 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
             switch self {
             case .initialized:
                 .initialized
-            case .attaching:
-                .attaching
+            case .attachingDueToAttachOperation:
+                .attaching(error: nil)
+            case let .attachingDueToContributorStateChange(error: error):
+                .attaching(error: error)
             case .attached:
                 .attached
             case .detaching:
@@ -173,7 +176,7 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
 
         fileprivate var operationID: UUID? {
             switch self {
-            case let .attaching(attachOperationID):
+            case let .attachingDueToAttachOperation(attachOperationID):
                 attachOperationID
             case let .detaching(detachOperationID):
                 detachOperationID
@@ -184,7 +187,8 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
                  .attached,
                  .detached,
                  .failed,
-                 .released:
+                 .released,
+                 .attachingDueToContributorStateChange:
                 nil
             }
         }
@@ -511,7 +515,7 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
         case .released:
             // CHA-RL1c
             throw ARTErrorInfo(chatError: .roomIsReleased)
-        case .initialized, .suspended, .attaching, .detached, .detaching, .failed:
+        case .initialized, .suspended, .attachingDueToAttachOperation, .attachingDueToContributorStateChange, .detached, .detaching, .failed:
             break
         }
 
@@ -521,7 +525,7 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
         }
 
         // CHA-RL1e
-        changeStatus(to: .attaching(attachOperationID: operationID))
+        changeStatus(to: .attachingDueToAttachOperation(attachOperationID: operationID))
 
         // CHA-RL1f
         for contributor in contributors {
@@ -622,7 +626,7 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
         case .failed:
             // CHA-RL2d
             throw ARTErrorInfo(chatError: .roomInFailedState)
-        case .initialized, .suspended, .attaching, .attached, .detaching:
+        case .initialized, .suspended, .attachingDueToAttachOperation, .attachingDueToContributorStateChange, .attached, .detaching:
             break
         }
 
@@ -712,7 +716,7 @@ internal actor RoomLifecycleManager<Contributor: RoomLifecycleContributor> {
             // See note on waitForCompletionOfOperationWithID for the current need for this force try
             // swiftlint:disable:next force_try
             return try! await waitForCompletionOfOperationWithID(releaseOperationID, waitingOperationID: operationID)
-        case .initialized, .attached, .attaching, .detaching, .suspended, .failed:
+        case .initialized, .attached, .attachingDueToAttachOperation, .attachingDueToContributorStateChange, .detaching, .suspended, .failed:
             break
         }
 

Sources/AblyChat/RoomStatus.swift
@@ -7,7 +7,7 @@ public protocol RoomStatus: AnyObject, Sendable {
 
 public enum RoomLifecycle: Sendable, Equatable {
     case initialized
-    case attaching
+    case attaching(error: ARTErrorInfo?)
     case attached
     case detaching
     case detached
@@ -20,6 +20,14 @@ public enum RoomLifecycle: Sendable, Equatable {
     //
     // 1. testing (e.g.  `#expect(status.isFailed)`)
     // 2. testing that a status does _not_ have a particular case (e.g. if !status.isFailed), which a `case` statement cannot succinctly express
+
+    public var isAttaching: Bool {
+        if case .attaching = self {
+            true
+        } else {
+            false
+        }
+    }
 
     public var isSuspended: Bool {
         if case .suspended = self {

Tests/AblyChatTests/Helpers/Subscription+RoomStatusChange.swift
@@ -12,6 +12,13 @@ extension Subscription where Element == RoomStatusChange {
         var error: ARTErrorInfo
     }
 
+    struct StatusChangeWithOptionalError {
+        /// A status change whose `current` has an optional associated error; ``error`` provides access to this error.
+        var statusChange: RoomStatusChange
+        /// The error associated with `statusChange.current`.
+        var error: ARTErrorInfo?
+    }
+
     func suspendedElements() async -> AsyncCompactMapSequence<Subscription<RoomStatusChange>, Subscription<RoomStatusChange>.StatusChangeWithError> {
         compactMap { statusChange in
             if case let .suspended(error) = statusChange.current {
@@ -26,6 +33,16 @@ extension Subscription where Element == RoomStatusChange {
         compactMap { statusChange in
             if case let .failed(error) = statusChange.current {
                 StatusChangeWithError(statusChange: statusChange, error: error)
+            } else {
+                nil
+            }
+        }
+    }
+
+    func attachingElements() async -> AsyncCompactMapSequence<Subscription<RoomStatusChange>, Subscription<RoomStatusChange>.StatusChangeWithOptionalError> {
+        compactMap { statusChange in
+            if case let .attaching(error) = statusChange.current {
+                StatusChangeWithOptionalError(statusChange: statusChange, error: error)
             } else {
                 nil
             }

Tests/AblyChatTests/RoomLifecycleManagerTests.swift
@@ -187,9 +187,9 @@ struct RoomLifecycleManagerTests {
         async let _ = try await manager.performAttachOperation()
 
         // Then: It emits a status change to ATTACHING, and its current state is ATTACHING
-        #expect(try #require(await statusChange).current == .attaching)
+        #expect(try #require(await statusChange).current == .attaching(error: nil))
 
-        #expect(await manager.current == .attaching)
+        #expect(await manager.current == .attaching(error: nil))
 
         // Post-test: Now that we’ve seen the ATTACHING state, allow the contributor `attach` call to complete
         contributorAttachOperation.complete(result: .success)
@@ -848,7 +848,7 @@ struct RoomLifecycleManagerTests {
         // Given: A RoomLifecycleManager, with a room lifecycle operation in progress
         let contributor = createContributor()
         let manager = await createManager(
-            forTestingWhatHappensWhenCurrentlyIn: .attaching(attachOperationID: UUID()), // case and ID arbitrary, just care that an operation is in progress
+            forTestingWhatHappensWhenCurrentlyIn: .attachingDueToAttachOperation(attachOperationID: UUID()), // case and ID arbitrary, just care that an operation is in progress
             contributors: [contributor]
         )
 
@@ -910,7 +910,7 @@ struct RoomLifecycleManagerTests {
         // Given: A RoomLifecycleManager, with a room lifecycle operation in progress
         let contributor = createContributor()
         let manager = await createManager(
-            forTestingWhatHappensWhenCurrentlyIn: .attaching(attachOperationID: UUID()), // case and ID arbitrary, just care that an operation is in progress
+            forTestingWhatHappensWhenCurrentlyIn: .attachingDueToAttachOperation(attachOperationID: UUID()), // case and ID arbitrary, just care that an operation is in progress
             contributors: [contributor]
         )
 