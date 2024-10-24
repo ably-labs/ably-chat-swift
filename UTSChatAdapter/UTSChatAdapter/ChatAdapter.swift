54f35f46e8cb2a24089ae33f6bb97c8f3001efdc
Marat Al <m.alekperov@gmail.com>
2024-10-25 00:04:43 +0200

    Added some custom implementations.

--- UTSChatAdapter/UTSChatAdapter/ChatAdapter.swift
+++ UTSChatAdapter/UTSChatAdapter/ChatAdapter.swift
@@ -50,6 +50,65 @@ class ChatAdapter {
         // GENERATED CONTENT BEGIN
         // ...
         // GENERATED CONTENT END
+        
+        // Custom fields implementation (see `Schema.skipPaths` for reasons):
+        
+        case "ChatClient":
+            let requestId = rpcParams["id"] as! String
+            let chatOptions = ClientOptions.from(rpcParams["clientOptions"])
+            let realtimeOptions = ARTClientOptions.from(rpcParams["realtimeClientOptions"])
+            let realtime = ARTRealtime(options: realtimeOptions)
+            let chatClient = DefaultChatClient(realtime: realtime, clientOptions: chatOptions)
+            let instanceId = generateId()
+            idToChatClient[instanceId] = chatClient
+            return jsonRpcResult(requestId, "{\"instanceId\":\"\(instanceId)\"}")
+            
+        // This field is optional and should be included in a corresponding json schema for automatic generation
+        case "ConnectionStatus#error":
+            let refId = rpcParams["refId"] as! String
+            guard let instance = idToConnectionStatus[refId] else {
+                print("Instance with this `refId` doesn't exist."); return nil;
+            }
+            if let error = instance.error { // ErrorInfo
+                return jsonRpcResult(rpcParams["id"] as! String, "{\"response\": \"\(error.json())\"}")
+            } else {
+                return jsonRpcResult(rpcParams["id"] as! String, "{\"response\": \(NSNull()) }")
+            }
+            
+        // This field is optional and should be included in a corresponding json schema for automatic generation
+        case "Message#createdAt":
+            let refId = rpcParams["refId"] as! String
+            guard let message = idToMessage[refId] else {
+                print("Message with `refId == \(refId)` doesn't exist.")
+                return nil
+            }
+            if let createdAt = message.createdAt { // number
+                return jsonRpcResult(rpcParams["id"] as! String, "{\"response\": \"\(createdAt)\"}")
+            } else {
+                return jsonRpcResult(rpcParams["id"] as! String, "{\"response\": \(NSNull()) }")
+            }
+            
+        // `events` is an array of strings in schema file which is not enougth for param auto-generation (should be `PresenceEventType`)
+        case "Presence.subscribe_eventsAndListener":
+            guard let events = rpcParams["events"] as? [String] else {
+                return nil
+            }
+            guard let presenceRef = idToPresence[rpcParams.refId] else {
+                print("Presence with `refId == \(rpcParams.refId)` doesn't exist.")
+                return nil
+            }
+            let subscription = await presenceRef.subscribe(events: events.map { PresenceEventType.from($0) })
+            let callback: (PresenceEvent) -> Void = {
+                self.webSocket.send(text: jsonRpcCallback(rpcParams.callbackId, "\($0.json())"))
+            }
+            Task {
+                for await event in subscription {
+                    callback(event)
+                }
+            }
+            let resultRefId = generateId()
+            idToPresenceSubscription[resultRefId] = subscription
+            return jsonRpcResult(rpcParams.requestId, "{\"refId\":\"\(resultRefId)\"}")
             
         default:
             print("Unknown method provided.")
